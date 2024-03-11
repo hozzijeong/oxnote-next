e97522a07799a21836d3443c1dc5c0c0a4f791cb
hozzijeong <ghwls0905@naver.com>
2024-03-11 15:26:34 +0900

    feat: type 및 상수 설정

---
+++ src/constants/form.ts
@@ -0,0 +1,7 @@
+import { UserAnswer, UserAnswerOption } from '@/app/types/form';
+
+export const ANSWER_OPTIONS: UserAnswerOption = {
+	[UserAnswer.YES]: '예',
+	[UserAnswer.NO]: '아니오',
+	[UserAnswer.NONE]: '상관 없어요',
+};
