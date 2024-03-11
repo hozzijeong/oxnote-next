7d2570b20b9af00302a8efd344d4724cc1e8cab0
hozzijeong <ghwls0905@naver.com>
2024-03-11 13:00:18 +0900

    chore: global css 설정 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const nextConfig = {
+	reactStrictMode: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+		prependData: `@import "src/styles/variables.scss"; @import "src/styles/mixins.scss";`,
+	},
+};
 
 export default nextConfig;
