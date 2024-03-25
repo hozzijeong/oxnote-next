66c48753061fc32163a0b2f5033bd2a160b21cad
hozzijeong <ghwls0905@naver.com>
2024-03-25 14:57:50 +0900

    refactor: belowWidth훅 node 환경에 맞게 수정

--- src/hooks/useIsBelowWidth.ts
+++ src/hooks/useIsBelowWidth.ts
@@ -1,22 +1,17 @@
-'use client';
-
-import { useCallback, useState } from 'react';
-import useResizeObserver from './useResizeObserver';
+import { useCallback, useEffect, useState } from 'react';
 
 const useIsBelowWidth = (width: number) => {
-	const [isBelowWidth, setIsBelowWidth] = useState<boolean>(
-		window.innerWidth < width
-	);
-
-	const windowWidthChangeCallback = useCallback(
-		(entries: ResizeObserverEntry[]) => {
-			const windowWidth = entries[0].contentRect.width;
-			setIsBelowWidth(windowWidth < width);
-		},
+	const [isBelowWidth, setIsBelowWidth] = useState(false);
+	const handleResize = useCallback(
+		() => setIsBelowWidth(width > window.innerWidth),
 		[width]
 	);
 
-	useResizeObserver({ resizeObserverCallback: windowWidthChangeCallback });
+	useEffect(() => {
+		handleResize();
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, [handleResize]);
 
 	return isBelowWidth;
 };
