8731194091bd165c6f84ded69dab87941716e32c
hozzijeong <ghwls0905@naver.com>
2024-04-19 22:14:05 +0900

    feat: 비로그인시 로그인 페이지로 redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 import { URL_PATH } from '@/constants/path';
-import { useRouter } from 'next/router';
+import { auth } from '@/lib/firebase';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function Home() {
 	const router = useRouter();
+
 	useEffect(() => {
 		router.push(URL_PATH.QUIZ_FORM);
 	}, [router]);
