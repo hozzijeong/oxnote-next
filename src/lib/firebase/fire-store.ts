a8410d16565142e8d875c9078f410fa2c88cc76e
hozzijeong <ghwls0905@naver.com>
2024-04-23 16:52:38 +0900

    refactor: 데이터 불러올 때 converter 필수로 작성하도록 설정

--- src/lib/firebase/fire-store.ts
+++ src/lib/firebase/fire-store.ts
@@ -13,6 +13,8 @@ import {
 	QueryFilterConstraint,
 	where,
 	DocumentSnapshot,
+	DocumentReference,
+	FirestoreDataConverter,
 } from 'firebase/firestore';
 import { app } from '.';
 
@@ -27,13 +29,45 @@ type WithMergeOption = MutateDocumentPathParams & {
 
 const db = getFirestore(app);
 
-// NOTE: 새로운 데이터를 '추가'하는 메서드
+// NOTE: 중첩된 문서 경로에 컬렉션을 추가하는 메서드. data 값을 추가한다면, 생성과 동시에 새로운 값을 추가할 수 있다.
 export const createCollection = async ({
 	path,
+	key,
 	data,
-}: MutateDocumentPathParams): Promise<void> => {
+}: {
+	path: string;
+	key: string;
+	data: DocumentData;
+}): Promise<DocumentReference<DocumentData, DocumentData>> => {
+	try {
+		const docRef = doc(db, path);
+
+		const docSnap = await getDoc(docRef);
+
+		if (!docSnap.exists()) {
+			throw new Error('존재하지 않는 경로입니다.');
+		}
+
+		const newCollectionRef = collection(docRef, key);
+		const createdData = await addDoc(newCollectionRef, data);
+
+		return createdData;
+	} catch (e) {
+		throw e;
+	}
+};
+
+// NOTE: 컬렉션에 문서를 추가하는 메서드. 단, 데이터의 id를 임의로 설정할 수 있다.
+export const createCollectionDocument = async ({
+	path,
+	data,
+}: MutateDocumentPathParams): Promise<
+	DocumentReference<DocumentData, DocumentData>
+> => {
 	try {
 		const docRef = await addDoc(collection(db, `${path}`), data);
+
+		return docRef;
 	} catch (e) {
 		throw new Error('새 문서를 생성하는 데 실패했습니다.');
 	}
@@ -55,12 +89,11 @@ export const updateDocumentData = async ({
 };
 
 // NOTE: 특정 경로에 있는 문서를 읽는 메서드.
-export const getDocumentSnapshot = async (
-	path: string
-): Promise<DocumentSnapshot<DocumentData, DocumentData>> => {
-	const categorySnapShot = await getDoc(doc(db, `${path}`));
-
-	return categorySnapShot;
+export const getDocumentSnapshot = async <T>(
+	path: string,
+	converter: FirestoreDataConverter<T>
+): Promise<DocumentSnapshot<T, DocumentData>> => {
+	return await getDoc(doc(db, `${path}`).withConverter(converter));
 };
 
 type FilterParams = {
