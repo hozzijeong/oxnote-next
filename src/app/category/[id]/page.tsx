0d653c48cb89331515691798122af7962217a71f
hozzijeong <ghwls0905@naver.com>
2024-05-08 15:45:26 +0900

    feat: 카테고리에 해당하는 quizList 구현

---
+++ src/app/category/[id]/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { http } from '@/lib/api';
+import { useEffect, useState } from 'react';
+import QuizItem from '@/components/quiz-list-item/QuizListItem';
+import { QuizListResponse } from '@/app/api/quiz/quiz.type';
+
+// NOTE: 자체 라우터를 만들면 안되는 이유 (https://github.com/vercel/next.js/discussions/54840)
+
+// TODO: 해당 페이지 자체를 컴포넌트화 가능하다고 생각. [id]에 해당하는 값만 어떻게 처리하면 좋을 듯
+const CategoryQuizList = ({ params: { id } }: { params: { id: string } }) => {
+	const [quizList, setQuizList] = useState<QuizListResponse[]>([]);
+
+	useEffect(() => {
+		const getQuizData = async () => {
+			const data = await http.get<QuizListResponse[]>(
+				`/api/quiz?category-id=${id}`
+			);
+
+			if (data.message === 'FAILURE') {
+				throw new Error(data.errors.message);
+			}
+
+			setQuizList(data.data);
+		};
+
+		getQuizData();
+	}, [id]);
+
+	return (
+		<div>
+			{quizList.length &&
+				quizList.map((quiz) => <QuizItem key={quiz.id} item={quiz} />)}
+		</div>
+	);
+};
+
+export default CategoryQuizList;
