0bb331de724746c6621ea039398f988923ba7be7
hozzijeong <ghwls0905@naver.com>
2024-05-16 18:51:37 +0900

    refactor: 카테고리 생성 훅 추가

--- src/app/category/hooks/useCreateCategory.ts
+++ src/app/category/hooks/useCreateCategory.ts
@@ -1,11 +1,24 @@
 import { http } from '@/lib/api';
 import { Category } from '../types';
 import { useCallback } from 'react';
+import useSWRMutation from 'swr/mutation';
+import { Key } from 'swr';
 
 export const useCreateCategory = () => {
-	const createCategory = useCallback((params: Category) => {
-		return http.post<null, Category>('/api/category', params);
-	}, []);
+	const { trigger, isMutating, reset } = useSWRMutation<
+		null,
+		Error,
+		Key,
+		Category
+	>('/api/category', http.post);
 
-	return createCategory;
+	const createCategory = useCallback(
+		async (params: Category) => {
+			const result = await trigger(params);
+			return result;
+		},
+		[trigger]
+	);
+
+	return { createCategory, isMutating, reset };
 };
