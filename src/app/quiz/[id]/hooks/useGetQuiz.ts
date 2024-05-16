3dbc7cda8b941e536bb526db1b44aca71e46adde
hozzijeong <ghwls0905@naver.com>
2024-05-16 16:15:39 +0900

    refactor: category, quizinfo 데이터 얻어오는 훅들 swr 적용

--- src/app/quiz/[id]/hooks/useGetQuiz.ts
+++ src/app/quiz/[id]/hooks/useGetQuiz.ts
@@ -1,37 +1,11 @@
 import { http } from '@/lib/api';
 import { QuizInfo } from '@/types';
-import { useEffect, useState } from 'react';
-
-let isFirst = true;
+import useSWR from 'swr';
 
 const useGetQuiz = (id: string) => {
-	const [quiz, setQuiz] = useState<QuizInfo | null>(null);
-	const [loading, setLoading] = useState(false);
-
-	useEffect(() => {
-		const getQuiz = async () => {
-			setLoading(true);
-			const data = await http.get<QuizInfo>(`/api/quiz/${id}`);
-
-			if (data.message === 'FAILURE') {
-				throw new Error(data.errors.message);
-			}
-
-			if (isFirst) {
-				setQuiz(data.data);
-				setLoading(false);
-				isFirst = false;
-			}
-		};
-
-		getQuiz();
-	}, [id]);
-
-	if (loading) {
-		throw new Promise(() => {});
-	}
-
-	return quiz;
+	return useSWR(`/api/quiz/${id}`, http.get<QuizInfo>, {
+		suspense: true,
+	});
 };
 
 export default useGetQuiz;
