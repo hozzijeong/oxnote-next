0fb00d5418cbeaa059d7241d86e2e585aeca2db7
hozzijeong <ghwls0905@naver.com>
2024-05-16 14:16:33 +0900

    refactor: layout 중첩으로 인해 부모 경로에 있는 layout을 page로 분리

--- src/app/quiz/[id]/page.tsx
+++ src/app/quiz/[id]/page.tsx
@@ -2,32 +2,24 @@
 
 import useToggle from '@/hooks/useToggle';
 import styles from './quiz-detail.module.scss';
-import { FavoriteButton } from '@/components';
-import { MouseEventHandler, useEffect, useState } from 'react';
+import { FavoriteButton, Header, Navbar, Pagination } from '@/components';
+import { Fragment, MouseEventHandler, useEffect, useState } from 'react';
 import { QuizInfo } from '@/types/quiz';
 import { http } from '@/lib/api';
 import type { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
 import { useUpdateQuizProperty } from '../hooks';
+import { useSessionStorage } from '@/hooks';
+import { useModifyQuiz } from './hooks';
+import useGetQuiz from './hooks/useGetQuiz';
 
 const QuizDetail = ({ params: { id } }: Params) => {
+	const [quizIds] = useSessionStorage<string[]>('quiz-id', []);
+	const { modifyHandler, deleteHandler } = useModifyQuiz(id);
+
 	const { isOn: explainOn, toggle: explainHandler } = useToggle();
 	const updateQuiz = useUpdateQuizProperty({ quizId: id });
 
-	const [quiz, setQuiz] = useState<QuizInfo | null>(null);
-
-	useEffect(() => {
-		const getQuiz = async () => {
-			const data = await http.get<QuizInfo>(`/api/quiz/${id}`);
-
-			if (data.message === 'FAILURE') {
-				throw new Error(data.errors.message);
-			}
-
-			setQuiz(data.data);
-		};
-
-		getQuiz();
-	}, [id]);
+	const quiz = useGetQuiz(id);
 
 	const submitAnswerHandler =
 		(answer: boolean): MouseEventHandler<HTMLButtonElement> =>
@@ -47,39 +39,59 @@ const QuizDetail = ({ params: { id } }: Params) => {
 	};
 
 	return (
-		<section className={styles['box']}>
-			<div className={styles['quiz-container']}>
-				<p className={styles['paragraph']}>{quiz?.title}</p>
-				<FavoriteButton
-					isFavorite={Boolean(quiz?.favorite)}
-					onClick={favoriteClickHandler}
-				/>
-			</div>
+		<Fragment>
+			<Header>
+				<Header.BackButton backUrl={-1} />
+				<Header.Title title='문제 풀기' />
+				<Header.Menu>
+					<button type='button' onClick={modifyHandler}>
+						수정하기
+					</button>
+					<button type='button' onClick={deleteHandler}>
+						삭제하기
+					</button>
+				</Header.Menu>
+			</Header>
+			<Pagination currentPosition={id} positions={quizIds} path={`/quiz`} />
+			<main className={styles.main}>
+				<section className={styles['box']}>
+					<div className={styles['quiz-container']}>
+						<p className={styles['paragraph']}>{quiz?.title}</p>
+						<FavoriteButton
+							isFavorite={Boolean(quiz?.favorite)}
+							onClick={favoriteClickHandler}
+						/>
+					</div>
 
-			<div className={styles['answer-container']}>
-				<button type='button' onClick={explainHandler}>{`해설 ${
-					explainOn ? '닫기' : '보기'
-				}`}</button>
-				{explainOn && <p className={styles['paragraph']}>{quiz?.explain}</p>}
-			</div>
+					<div className={styles['answer-container']}>
+						<button type='button' onClick={explainHandler}>{`해설 ${
+							explainOn ? '닫기' : '보기'
+						}`}</button>
+						{explainOn && (
+							<p className={styles['paragraph']}>{quiz?.explain}</p>
+						)}
+					</div>
 
-			<div className={styles['button-container']}>
-				<button
-					className={`${styles.button} ${styles['false-bg']}`}
-					type='button'
-					onClick={submitAnswerHandler(false)}
-				>
-					X
-				</button>
-				<button
-					className={`${styles.button} ${styles['true-bg']}`}
-					type='button'
-					onClick={submitAnswerHandler(true)}
-				>
-					O
-				</button>
-			</div>
-		</section>
+					<div className={styles['button-container']}>
+						<button
+							className={`${styles.button} ${styles['false-bg']}`}
+							type='button'
+							onClick={submitAnswerHandler(false)}
+						>
+							X
+						</button>
+						<button
+							className={`${styles.button} ${styles['true-bg']}`}
+							type='button'
+							onClick={submitAnswerHandler(true)}
+						>
+							O
+						</button>
+					</div>
+				</section>
+			</main>
+			<Navbar />
+		</Fragment>
 	);
 };
 
