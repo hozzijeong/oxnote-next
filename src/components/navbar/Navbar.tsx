fdff538fbd77186ee99f63bdd1870fba46a746ac
hozzijeong <ghwls0905@naver.com>
2024-05-30 19:34:42 +0900

    refactor: navbar replace 제거

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,29 +9,25 @@ const Navbar = () => {
 	return (
 		<nav className={styles.wrapper}>
 			<div>
-				<Link
-					className={styles.link}
-					href={URL_PATH.QUIZ_FILTER}
-					replace={true}
-				>
+				<Link className={styles.link} href={URL_PATH.QUIZ_FILTER}>
 					<Image src={Quiz} alt='문제 풀기' width={24} height={24} />
 					문제 풀기
 				</Link>
 			</div>
 			<div>
-				<Link className={styles.link} href={URL_PATH.QUIZ_FORM} replace={true}>
+				<Link className={styles.link} href={URL_PATH.QUIZ_FORM}>
 					<Image src={Plus} alt='문제 등록하기' width={24} height={24} />
 					문제 등록하기
 				</Link>
 			</div>
 			<div>
-				<Link className={styles.link} href={URL_PATH.CATEGORY} replace={true}>
+				<Link className={styles.link} href={URL_PATH.CATEGORY}>
 					<Image src={Folder} alt='모아보기' width={24} height={24} />
 					모아보기
 				</Link>
 			</div>
 			<div>
-				<Link className={styles.link} href={URL_PATH.MY_PAGE} replace={true}>
+				<Link className={styles.link} href={URL_PATH.MY_PAGE}>
 					<Image src={Profile} alt='마이페이지' width={24} height={24} />
 					마이페이지
 				</Link>
