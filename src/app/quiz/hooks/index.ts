b01c79073e47741708bb24ca7ecd813a2c83af9d
hozzijeong <ghwls0905@naver.com>
2024-05-30 16:58:38 +0900

    refactor: quiz 업데이트 프로퍼티 추가

--- src/app/quiz/hooks/index.ts
+++ src/app/quiz/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useUpdateQuizProperty';
+export * from './useGetQuizList';
--- src/app/quiz/hooks/useUpdateQuizProperty.ts
+++ src/app/quiz/hooks/useUpdateQuizProperty.ts
@@ -4,20 +4,21 @@ import { useCallback } from 'react';
 import { Key } from 'swr';
 import useSWRMutation from 'swr/mutation';
 
-type UpdateAbleQuizProperty = Pick<QuizInfo, 'favorite' | 'record'>;
-
-type UpdateParameter = Partial<UpdateAbleQuizProperty>;
+type UpdateAbleQuizProperty =
+	| Pick<QuizInfo, 'favorite'>
+	| Pick<QuizInfo, 'record'>
+	| Omit<QuizInfo, 'record' | 'categoryId' | 'id'>;
 
 export const useUpdateQuizProperty = (id: string) => {
 	const { trigger, isMutating, reset } = useSWRMutation<
 		void,
 		Error,
 		Key,
-		UpdateParameter
+		UpdateAbleQuizProperty
 	>(`/api/quiz/${id}`, http.patch);
 
 	const updateQuiz = useCallback(
-		async (updateProperty: UpdateParameter) => {
+		async (updateProperty: UpdateAbleQuizProperty) => {
 			const result = await trigger(updateProperty);
 			return result;
 		},
