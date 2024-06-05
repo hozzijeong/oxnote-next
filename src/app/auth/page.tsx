0abe60f5804594a85be3a6907aef0b851a0ed7bc
hozzijeong <ghwls0905@naver.com>
2024-06-05 17:38:29 +0900

    refactor: 로그인 실패시 메세지 호출

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -5,9 +5,12 @@ import { signInWithGoogle } from '@/lib/firebase';
 import { useRouter } from 'next/navigation';
 import { URL_PATH } from '@/constants/path';
 import { useLogin } from './hooks';
+import { useToast } from '@/components/toast';
+import { CustomError } from '@/lib/error';
 
 const Auth = () => {
 	const router = useRouter();
+	const addToast = useToast();
 	const { login } = useLogin({
 		onSuccess: () => router.push(URL_PATH.CATEGORY),
 	});
@@ -19,9 +22,15 @@ const Auth = () => {
 			const result = await signInWithGoogle();
 			const { uid, displayName, email } = result.user;
 
-			login({ uid, email, userName: displayName });
+			await login({ uid, email, userName: displayName });
 		} catch (e) {
-			console.error(e);
+			let message = '로그인하는데 실패했습니다. 다시 시도해주세요';
+			if (e instanceof CustomError) {
+				message = e.message;
+			}
+			addToast({
+				message,
+			});
 		}
 	};
 
