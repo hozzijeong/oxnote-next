c2bdd6f33b73caceea0e686e034a430b94230145
hozzijeong <ghwls0905@naver.com>
2024-06-07 16:23:23 +0900

    feat: 로그아웃 기능 구현

---
+++ src/app/my-page/hooks/index.ts
@@ -0,0 +1,2 @@
+export * from './useAccountDelete';
+export * from './useLogout';
--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,20 +1,13 @@
 'use client';
 
-import { useToast } from '@/components/toast';
-import { getAuth, signOut } from 'firebase/auth';
 import { MouseEventHandler } from 'react';
+import { useLogout } from './hooks/useLogout';
 
 const MyPage = () => {
-	const addToast = useToast();
+	const { logout } = useLogout();
 
-	const logoutHandler: MouseEventHandler<HTMLButtonElement> = async () => {
-		const auth = getAuth();
-		try {
-			await signOut(auth);
-		} catch (error) {
-			addToast({ message: '로그아웃에 실패했습니다' });
-		}
-	};
+	const logoutHandler: MouseEventHandler<HTMLButtonElement> = () => logout();
+
 	return (
 		<section>
 			<div>
